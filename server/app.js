9288c7963ab6b44cf173dac6c4b0d397b552feb4
ritlambdas <ritlambdas@gmail.com>
2020-02-08 14:32:46 -0500

    Mongoose Setup

--- server/app.js
+++ server/app.js
@@ -20,9 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+mongoose.connect('mongodb+srv://prepbrick:hackmybrickyounerd@cluster0-npdbw.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
